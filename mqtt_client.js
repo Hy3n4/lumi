6c3160f3c3b9e41cc433ae6b64f1711a3c362773
Chadima Patrik <Patrik.Chadima@cdis.cz>
2022-03-18 13:37:36 +0100

    Fixed MQTT_HOSTNAME in install.sh

--- mqtt_client.js
+++ mqtt_client.js
@@ -21,6 +21,7 @@ mqtt_client.on('connect', () => {
 mqtt_client.on('message', (topic, message) => {
     common.myLog('Received topic= ' + topic, common.colors.yellow);
     common.myLog('message = ' + message);
+    common.myLog(topic.split("/")[1]);
     try {
         switch (topic.split("/")[1]) {
             case 'light':
