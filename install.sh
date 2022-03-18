f402ba6b3eac3222029983e28a4b31b17fc2d87f
Chadima Patrik <Patrik.Chadima@cdis.cz>
2022-03-18 09:54:25 +0100

    Added simple install script

---
+++ install.sh
@@ -0,0 +1,88 @@
+#!/bin/ash
+# shellcheck shell=dash
+
+print_style () {
+
+    if [ "$2" = "info" ] ; then
+        COLOR="96m";
+    elif [ "$2" = "success" ] ; then
+        COLOR="92m";
+    elif [ "$2" = "warning" ] ; then
+        COLOR="93m";
+    elif [ "$2" = "danger" ] ; then
+        COLOR="91m";
+    else #default color
+        COLOR="0m";
+    fi
+
+    STARTCOLOR="\e[$COLOR";
+    ENDCOLOR="\e[0m";
+
+    printf "$STARTCOLOR%b$ENDCOLOR\n" "$1";
+}
+
+MQTT_SERVER="${MQTT_SERVER:-localhost}"
+MQTT_USERNAME="${MQTT_USERNAME:-mqtt}"
+MQTT_PASSWORD="${MQTT_PASSWORD:-password}"
+GIT_REPO_URL="https://github.com/Hy3n4/lumi.git"
+GIT_REPO_PATH="/opt/lumi"
+LOCALREPO_VC_DIR=$GIT_REPO_PATH/.git
+USE_MAC_IN_MQTT_TOPIC="true"
+if [ "${USE_MAC_IN_MQTT_TOPIC}" = "true" ]; then
+    MQTT_HOSTNAME=$(printf "%s_%s" "$(uname -n)" "$(ifconfig wlan0 | grep -o -E '([[:xdigit:]]{1,2}:){5}[[:xdigit:]]{1,2}' | sed 's/\://g')")
+else
+    MQTT_HOSTNAME=$(uname -n)
+fi
+print_style "Installing dependencies..." "info"
+print_style "Running opkg update." "info"; print_style "(This might take a while...)" "warning"
+# opkg update >/dev/null 2>&1
+print_style "Installing packages..." "info"
+# opkg install node git-http mpg123 mpc mpd-full >/dev/null 2>&1
+mkdir -p /opt
+cd /opt || { print_style "Cannot cd to /opt" "danger"; exit 1; }
+[ -d $LOCALREPO_VC_DIR ] || git clone $GIT_REPO_URL $GIT_REPO_PATH
+(cd $GIT_REPO_PATH; git pull $GIT_REPO_URL)
+cd $GIT_REPO_PATH || { printf '%b\n' "cannot cd to /opt/lumi"; exit 1; }
+print_style "Generating lumi config file..." "info"
+cat <<EOL > /opt/lumi/config.json
+{
+  "sensor_debounce_period": 300,
+  "sensor_treshhold": 50,
+  "button_click_duration": 300,
+          
+  "homeassistant": true,
+  "tts_cache": true,
+  "sound_channel": "Master",
+  "sound_volume": 50,
+  "mqtt_url": "mqtt://${MQTT_SERVER}",
+  "mqtt_topic": "${MQTT_HOSTNAME}",
+  "use_mac_in_mqtt_topic": ${USE_MAC_IN_MQTT_TOPIC},
+  "mqtt_options": {
+    "port": 1883,
+    "username": "${MQTT_USERNAME}",
+    "password": "${MQTT_PASSWORD}",
+    "keepalive": 60,
+    "reconnectPeriod": 1000,
+    "clean": true,
+    "encoding": "utf8",
+    "will": {
+      "topic": "${MQTT_HOSTNAME}/state",
+      "payload": "offline",
+      "qos": 1,
+      "retain": true
+    }
+  }
+}
+EOL
+print_style "Installing service..." "info"
+chmod +x $GIT_REPO_PATH/lumi
+cp $GIT_REPO_PATH/lumi /etc/init.d/lumi
+/etc/init.d/lumi enable
+/etc/init.d/lumi start
+LUMI_SERVICE_STATUS=$(/etc/init.d/lumi status)
+if [ "${LUMI_SERVICE_STATUS}" = "running" ]; then
+    print_style "lumi service is ${LUMI_SERVICE_STATUS}" "success"
+else
+    print_style "lumi service failed to start and is now ${LUMI_SERVICE_STATUS}" "danger"
+fi
+print_style "All done..." "success"
